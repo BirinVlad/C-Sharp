a42be8cb5732e0a49f0d9d2dde35ce16b4f81625
BirinVlad <xa4apur4ik@gmail.com>
2022-10-05 20:23:25 +0300

    Laba 3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorna_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x - значення Х
+            //y - значення У
+            int x, y;
+            
+            Console.WriteLine("Введiть значення X : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть значення Y : ");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x > 0 && y > 0) {
+                Console.WriteLine("Належить першому квадранту.");
+            }
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Належить третьому квадранту.");
+            }
+            if((x < 0 && y >0)||(x > 0 && y < 0))
+            {
+                Console.WriteLine("Неправельно !!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
